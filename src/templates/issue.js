1d8143bdf4ab795444834a0b45d4552af0f1bcd6
amcc <alistairmcclymont@gmail.com>
2019-05-31 12:29:57 +0100

    fix for issue

--- src/templates/issue.js
+++ src/templates/issue.js
@@ -10,24 +10,24 @@ const IssueTemplate = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1><Link to={`/issues/`}>Issues</Link> / {data.nodeIssue.title}</h1>
-      <div
-        dangerouslySetInnerHTML={{ __html: data.nodeIssue.body.processed }}
-      />
-
+      <h1>
+        <Link to={`/issues/`}>Issues</Link> / {data.nodeIssue.title}
+      </h1>
+      {data.nodeArticle.body && (
+        <div
+          dangerouslySetInnerHTML={{ __html: data.nodeIssue.body.processed }}
+        />
+      )}
       {data.nodeIssue.relationships.field_issue_media.map(
         ({ relationships }) => (
           <Img
-            key={
-              relationships.field_media_image.localFile.childImageSharp.id
-            }
+            key={relationships.field_media_image.localFile.childImageSharp.id}
             fluid={
               relationships.field_media_image.localFile.childImageSharp.fluid
             }
           />
         )
       )}
-
     </Layout>
   )
 }
